e9a75909e240e7ae6f71a921325b36d399a30651
Tseringdhoundup9813 <tseringlama9813694977@gmail.com>
2024-01-13 13:29:13 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+
+require('dotenv').config()
+
+const express = require('express');
+const app = express();
+
+
+
+
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,8 @@
+
+const app = require('./app.js')
+
+
+const port = process.env.PORT||5000
+app.listen(port,()=>{
+    console.log(`Server is running on PORT ${port}`)
+})
